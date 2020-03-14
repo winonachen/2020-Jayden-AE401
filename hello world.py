0fd5507665f5d59b69e57ab96b6bd53567d291ab
winonachen <62165743+winonachen@users.noreply.github.com>
2020-03-14 12:53:32 +0800

    hello world
    印出文字

---
+++ hello world.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Hello World")
+print("I'm Chen Zu Wei")
+print('she is "cool"')
+abc=input('請輸入你的名字:')
+print(abc,'你好呀!!!')
