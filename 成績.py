4f5a6e0173c117eea425798f4753b00936e40578
winonachen <62165743+winonachen@users.noreply.github.com>
2020-03-21 12:31:03 +0800

    AE401-2-成績
    有沒有及格

---
+++ 成績.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 11:28:58 2020
+
+@author: user
+"""
+
+
+a=input("請輸入你的考試成績:")
+a=float(a)
+if a>59:
+    print("恭喜你~!有及格!")
+else:
+    print("嗚~!沒及格!")
