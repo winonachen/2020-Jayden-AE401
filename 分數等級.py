ae485063d34540953d421599648cf2a5ae9ff54b
winonachen <62165743+winonachen@users.noreply.github.com>
2020-03-21 12:31:55 +0800

    AE401-2-分數等級
    分數等級

---
+++ 分數等級.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 12:13:25 2020
+
+@author: user
+"""
+
+
+a=input("請輸入你的考試成績:")
+a=float(a)
+if a>=90:
+    print("A")
+elif a<90 and a>=80:
+    print("B")
+else:
+    print("C")
