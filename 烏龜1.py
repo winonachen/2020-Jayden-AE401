d798d28e8073bca6c6e596af2da5864535078681
winonachen <62165743+winonachen@users.noreply.github.com>
2020-04-25 12:02:56 +0800

    烏龜1
    讓烏龜畫方形

---
+++ 烏龜1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 10:52:46 2020
+
+@author: user
+"""
+
+
+import turtle
+a=turtle.Turtle()
+b=turtle.Screen()
+b.title("My window")
+b.bgcolor ('purple')
+a.color ('white')
+a.shape ('turtle')
+a.forward(100)
+a.left(90)
+a.forward(100)
+a.left(90)
+a.forward(100)
+a.left(90)
+a.forward(100)
+a.left(90)
+turtle.done()
