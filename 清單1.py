d43c63ebc9c0cfdc8851bac99c2d034553ffbfff
winonachen <62165743+winonachen@users.noreply.github.com>
2020-05-02 12:00:01 +0800

    清單1
    清單1

---
+++ 清單1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 11:34:19 2020
+
+@author: user
+"""
+
+
+a=["羽球","游泳","桌球","走路"]
+print(a)
+a.append("圖書館")
+print(a)
+a.insert(2,123)
+print(a)
+a.remove("圖書館")
+print(a)
