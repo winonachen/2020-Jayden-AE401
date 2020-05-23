67a38ad3279a51737436db6a2650b41fd6cd8dc0
winonachen <62165743+winonachen@users.noreply.github.com>
2020-05-23 12:05:33 +0800

    add3
    add3

---
+++ add3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 11:27:05 2020
+
+@author: user
+"""
+
+#
+def add3 (num1,num2,num3):
+    total=num1*num2*num3
+    return total
+a=add3(12,12,12)
+print(a)
