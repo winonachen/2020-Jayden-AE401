9d7411b804bc003821123e0cfe1adea9b52c0e7c
winonachen <62165743+winonachen@users.noreply.github.com>
2020-05-28 20:27:38 +0800

    HW7
    HW7

---
+++ homework7.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 19:18:33 2020
+
+@author: user
+"""
+
+
+import random
+a=["你","我","他"]
+b=["在打","在踢","在投"]
+c=["排球","足球","籃球"]
+def ans (a):
+    total=random.sample(ans)
+    return total
+def ans (b):
+    total=random.sample(ans)
+    return total
+def ans (c):
+    total=random.sample(ans)
+    return total
+print(ans)
